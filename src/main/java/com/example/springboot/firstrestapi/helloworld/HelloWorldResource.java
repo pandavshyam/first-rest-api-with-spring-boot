897d603baa2469e1574d139ee201876a83e682a3
shyampandav <pandavshyam@gmail.com>
2023-07-08 12:37:28 +0530

    Setup

---
+++ src/main/java/com/example/springboot/firstrestapi/helloworld/HelloWorldResource.java
@@ -0,0 +1,19 @@
+package com.example.springboot.firstrestapi.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Controller
+@RestController
+public class HelloWorldResource {
+
+  @RequestMapping("/hello-world")
+  public String helloWorld(){
+    return "Hello World!";
+  }
+
+  @RequestMapping("/hello-world-bean")
+  public HelloWorldBean helloWorldBean(){
+    return new HelloWorldBean("Hello World!");
+  }
+}
