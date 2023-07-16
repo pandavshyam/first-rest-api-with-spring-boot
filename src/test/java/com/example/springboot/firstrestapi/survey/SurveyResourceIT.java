84b107a5288ed61737e9dd5c53ea7e12946878c3
shyampandav <pandavshyam@gmail.com>
2023-07-16 19:18:22 +0530

    Add test for retrieve all survey questions

--- src/test/java/com/example/springboot/firstrestapi/survey/SurveyResourceIT.java
+++ src/test/java/com/example/springboot/firstrestapi/survey/SurveyResourceIT.java
@@ -25,6 +25,7 @@ public class SurveyResourceIT {
       """;
 
   private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+  private static String ALL_SURVEY_QUESTIONS = "/surveys/Survey1/questions";
 
   @Autowired
   private TestRestTemplate template;
@@ -32,9 +33,23 @@ public class SurveyResourceIT {
   @Test
   void retrieveSpecificSurveyQuestion_basicScenario() throws JSONException {
     ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_QUESTION_URL, String.class);
-    String expectedResponse = """
+    String expectedResponse =
+    """
       {"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
-        """;
+    """;
+
+    assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+    assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+    JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), true);
+  }
+
+  @Test
+  void retrieveAllSurveyQuestions_basicScenario() throws JSONException {
+    ResponseEntity<String> responseEntity = template.getForEntity(ALL_SURVEY_QUESTIONS, String.class);
+    String expectedResponse =
+    """
+      [{"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"},{"id":"Question2","description":"Fastest Growing Cloud Platform","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"Google Cloud"},{"id":"Question3","description":"Most Popular DevOps Tool","options":["Kubernetes","Docker","Terraform","Azure DevOps"],"correctAnswer":"Kubernetes"}]
+    """;
 
     assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
     assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
