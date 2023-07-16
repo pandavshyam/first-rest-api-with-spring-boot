bba9de9a54cb72f93613a9b36e136d7872d8a91a
shyampandav <pandavshyam@gmail.com>
2023-07-16 13:24:55 +0530

    Use Spring Data JPA

---
+++ src/main/java/com/example/springboot/firstrestapi/user/UserDetailsCommandLineRunner.java
@@ -0,0 +1,35 @@
+package com.example.springboot.firstrestapi.user;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsCommandLineRunner implements CommandLineRunner {
+
+  private Logger logger = LoggerFactory.getLogger(getClass());
+
+  private UserDetailsRepository repository;
+
+  public UserDetailsCommandLineRunner(UserDetailsRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    logger.info(Arrays.toString(args).toString());
+    repository.save(new UserDetails("Shyam", "admin"));
+    repository.save(new UserDetails("Prashant", "normal_user"));
+    repository.save(new UserDetails("Kanchan", "admin"));
+    repository.save(new UserDetails("Sangita", "admin"));
+
+    // List<UserDetails> users = repository.findAll();
+    List<UserDetails> users = repository.findByRole("admin");
+
+    users.forEach(user -> logger.info(user.toString()));
+  }
+}
---
+++ src/main/java/com/example/springboot/firstrestapi/user/UserDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboot.firstrestapi.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
+  List<UserDetails> findByRole(String role);
+}
