21e96654c374a649fe9df2c608aeab9944dd552f
shyampandav <pandavshyam@gmail.com>
2023-07-16 15:37:44 +0530

    Add test case for retrieve specific question from the survey

---
+++ src/test/java/com/example/springboot/firstrestapi/survey/SurveyResourceIT.java
@@ -0,0 +1,39 @@
+package com.example.springboot.firstrestapi.survey;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class SurveyResourceIT {
+
+  String str = """
+    {
+      "id": "Question1",
+      "description": "Most Popular Cloud Platform Today",
+      "options": ["AWS", "Azure", "Google Cloud", "Oracle Cloud"],
+      "correctAnswer": "AWS"
+    }
+      """;
+
+  private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+
+  @Autowired
+  private TestRestTemplate template;
+
+  @Test
+  void retrieveSpecificSurveyQuestion_basicScenario(){
+    ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_QUESTION_URL, String.class);
+    String expectedResponse = """
+      {"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
+        """;
+    assertEquals(expectedResponse.trim(), responseEntity.getBody());
+    System.out.println(responseEntity.getBody());
+    System.out.println(responseEntity.getHeaders());
+  }
+}
