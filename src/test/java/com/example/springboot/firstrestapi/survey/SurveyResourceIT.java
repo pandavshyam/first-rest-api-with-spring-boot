fe4321fc926b6fc991fb8aee31c6c95ca3c9ec7d
shyampandav <pandavshyam@gmail.com>
2023-07-18 00:47:09 +0530

    Add test case for adding new question to survey

--- src/test/java/com/example/springboot/firstrestapi/survey/SurveyResourceIT.java
+++ src/test/java/com/example/springboot/firstrestapi/survey/SurveyResourceIT.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -26,6 +29,7 @@ public class SurveyResourceIT {
 
   private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
   private static String ALL_SURVEY_QUESTIONS = "/surveys/Survey1/questions";
+  private static String CREATE_NEW_QUESTION = "/surveys/Survey1/questions";
 
   @Autowired
   private TestRestTemplate template;
@@ -54,5 +58,39 @@ public class SurveyResourceIT {
     assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
     assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
     JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), true);
+  }
+
+  @Test
+  void addNewSurveyQuestion_basicScenario() throws JSONException {
+    // ResponseEntity<String> responseEntity = template.getForEntity(CREATE_NEW_QUESTION, String.class);
+    String requestBody =
+    """
+    {
+      "id": "Question 4",
+      "description": "Some text here",
+      "options": [
+          "AWS",
+          "Azure",
+          "Google Cloud",
+          "Oracle Cloud",
+          "Rackspace"
+      ],
+      "correctAnswer": "AWS"
+    }
+    """;
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "application/json");
+
+    HttpEntity<String> httpEntity = new HttpEntity<>(requestBody, headers);
+
+    ResponseEntity<String> responseEntity = template.exchange(CREATE_NEW_QUESTION, HttpMethod.POST, httpEntity, String.class);
+
+    assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+    String locationHeader = responseEntity.getHeaders().get("Location").get(0);
+    assertTrue(locationHeader.contains("/surveys/Survey1/questions"));
+    JSONAssert.assertEquals(null, responseEntity.getBody(), true);
+
+    // Delete
+    template.delete(locationHeader);
   }
 }
